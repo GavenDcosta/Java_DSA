246acc7b3747ad6a64605d0f32fe927e651aeefa
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-09 21:32:46 +0530

    counting occourrences

---
+++ Loops_and_Conditional/CountingOccurrences.java
@@ -0,0 +1,16 @@
+package Loops_and_Conditional;
+
+public class CountingOccurrences {
+    public static void main(String[] args) {
+        int n = 35554;
+        int count = 0;
+        while(n>0){
+            int rem = n%10;
+            if(rem == 5){
+                count ++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
