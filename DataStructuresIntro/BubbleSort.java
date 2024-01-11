4934d06ace65640e604855b23d2c6001415479e1
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-11 19:29:01 +0530

    bubble sort

--- DataStructuresIntro/BubbleSort.java
+++ DataStructuresIntro/BubbleSort.java
@@ -2,6 +2,46 @@ package DataStructuresIntro;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        
+        int nums[] = {6,5,2,8,9,4};
+
+        System.out.println("Before Sorting");
+        printArr(nums);
+
+        bubbleSort(nums);
+
+        System.out.println("After sorting");
+        printArr(nums);
+
     }
+
+    public static void printArr(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length - 1; j ++){
+               if(nums[j] > nums[j + 1]){
+                swap(nums, j, j+1);
+                
+                //  int temp = nums[j];
+                //  nums[j] = nums[j+1];
+                //  nums[j+1] = temp;
+               }
+            }
+        }
+    }
+
+
+    public static void swap(int[] nums, int firstIndex, int secondIndex){
+        int temp = nums[firstIndex];
+        nums[firstIndex] = nums[secondIndex];
+        nums[secondIndex] = temp;
+    }
+
 }
+
+
+// O(n^2)
