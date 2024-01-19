e3fa7f8e1cfd285a7ade4df4516da3626624549a
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-19 18:06:53 +0530

    Smallest Letter

---
+++ Problems/Binary_Search/SmallestLetter.java
@@ -0,0 +1,42 @@
+package Problems.Binary_Search;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char letters[] = {'c','f','j'};
+
+        char target = 'a';
+
+        char result = nextGreatestLetter(letters, target);
+
+        if(result != -1){
+            System.out.println("Element found at index : " + result);
+        }else{
+            System.out.println("Element not found");
+        }
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(letters[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+
+        }
+
+        return letters[start  % letters.length];
+    }
+
+}
+
+
+// O(log n)
+
+
