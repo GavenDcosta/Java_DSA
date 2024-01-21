3da20a46139051a450e47ad78cd67212185d141b
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-21 08:21:56 +0530

    checking

--- Problems/Binary_Search/RotatedBinarySearchDuplicateElements.java
+++ Problems/Binary_Search/RotatedBinarySearchDuplicateElements.java
@@ -2,7 +2,7 @@ package Problems.Binary_Search;
 
 public class RotatedBinarySearchDuplicateElements {
     public static void main(String[] args) {
-        int[] arr = {4,5,5,6,7,7,0,1,2};
+        int[] arr = {4,5,5,6,7,7,0,1,1,2};
         System.out.println(findPivot(arr));
 
         int target = 1;
