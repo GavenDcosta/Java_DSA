e051362b148450fd62b9952c50dcfe24de194c43
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-25 17:59:33 +0530

    richest customer wealth

---
+++ Problems/Loops/RichestCustomerWealth.java
@@ -0,0 +1,28 @@
+package Problems.Loops;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {
+            {1,2,3},
+            {3,2,1},
+        };
+
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int max = 0;
+
+        for(int i = 0; i<accounts.length; i++){
+            int sum = 0;
+            for(int j = 0; j < accounts[i].length; j++){
+                sum = sum + accounts[i][j];
+            }
+            if(sum > max){
+                max = sum;
+            }
+        }
+
+        return max;
+    }
+}
