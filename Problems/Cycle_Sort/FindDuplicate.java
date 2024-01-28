daae9e6cfa647460981566d4b56fe1d7393d07a9
Gaven Dcosta <gavendcosta@gmail.com>
2024-01-28 11:20:39 +0530

    find duplicate

--- Problems/Cycle_Sort/FindDuplicate.java
+++ Problems/Cycle_Sort/FindDuplicate.java
@@ -2,19 +2,27 @@ package Problems.Cycle_Sort;
 
 public class FindDuplicate {
     public static void main(String[] args) {
-        
+        int[] nums = {1,3,4,2,2};
+
+        System.out.println(cyclicSort(nums));
     }
 
-    static void cyclicSort(int[] arr){
+     static int cyclicSort(int[] arr){
         int i = 0; 
         while(i < arr.length){
-            int correctIndex = arr[i] - 1;
-            if(arr[i] != arr[correctIndex]){
-                swap(arr, i , correctIndex);
+
+            if(arr[i] != i+1){
+                int correctIndex = arr[i] - 1;
+                if(arr[i] != arr[correctIndex]){
+                    swap(arr, i , correctIndex);
+                }else{
+                    return arr[i];
+                }
             }else{
                 i++;
             }
         }
+        return -1;
     }
 
     static void swap(int[] arr, int i, int correctIndex){
