345e827226c275b9fb9207c0eecdc23e948059e4
Gaven Dcosta <gavendcosta@gmail.com>
2024-02-03 17:19:26 +0530

    pattern 2

--- Basics/Patterns/Pattern_2.java
+++ Basics/Patterns/Pattern_2.java
@@ -1,5 +1,34 @@
 package Basics.Patterns;
 
 public class Pattern_2 {
-    
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j<=i; j ++){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 1; i <= 4; i++){
+            for(int j = 4; j>=i; j --){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
 }
+
+
+// 1
+// 12
+// 123
+// 1234
+
+// 4321
+// 432
+// 43
+// 4
