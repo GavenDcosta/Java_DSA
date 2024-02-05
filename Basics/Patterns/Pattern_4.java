2f1e32288b54ca6fd76ff23cb19d58847dec3fba
Gaven Dcosta <gavendcosta@gmail.com>
2024-02-05 17:35:11 +0530

    pattern 4

---
+++ Basics/Patterns/Pattern_4.java
@@ -0,0 +1,19 @@
+package Basics.Patterns;
+
+public class Pattern_4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 8; i ++ ){
+            int totalCount = i > 4 ? 2 * 4 - i : i;
+
+            int noOfSpaces = 4 - totalCount;
+            for(int s = 1; s <= noOfSpaces; s++){
+               System.out.print(" ");
+            }
+
+            for(int j = 1; j <= totalCount; j ++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
