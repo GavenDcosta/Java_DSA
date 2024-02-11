e339136d84bc1616a0535e7ce65571b7e6e8871b
Gaven Dcosta <gavendcosta@gmail.com>
2024-02-11 10:35:18 +0530

    magic no

---
+++ Problems/BitWise_Operators/Magic_Number.java
@@ -0,0 +1,20 @@
+package Problems.BitWise_Operators;
+
+public class Magic_Number{
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = 0;
+        int base = 5;
+
+        while(n > 0){
+              int last = n & 1;
+              n = n >> 1;
+
+              ans += last * base;
+
+              base = base * 5;  // 5^0 + 5^1 + 5^2 .....
+        }
+
+        System.out.println(ans);
+    }
+}
